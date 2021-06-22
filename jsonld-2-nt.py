03a8e423f18be9256f0d273943527a5850ffaf0f
eknutov <eknutov@gmail.com>
2021-06-23 01:00:01 +0200

    added jsonld-2-nt script

---
+++ jsonld-2-nt.py
@@ -0,0 +1,46 @@
+# =================================================================
+#   Transform directory of JSON-LD files into one N-Triples file
+#   (files in the directory need to have `.jsonld` extension)
+#   arguments:
+#   [1] - directory with the json-ld files
+#   output:
+#   `output_NT_file.nt` - output N-Triples file
+# =================================================================
+import sys
+import rdflib
+import os
+import requests
+
+
+if(len(sys.argv) < 2):
+    print('No json-ld directory is provided, please try again')
+    sys.exit()
+
+input_dir = sys.argv[1] + "/"   # directory to list and iterate files
+input_file_list = []
+output_file = os.getcwd() + "/" + "output_NT_file.nt"
+
+# create the list of only json-ld files in the provided directory to process
+for fn in os.listdir(input_dir):
+    if fn.endswith(".jsonld"):
+        input_file_list.append(input_dir+fn)
+
+print("====================================================================")
+
+nt_graph = rdflib.Graph()    # create an RDF graph to write triples statements to
+
+for f in input_file_list:
+    try:
+        g = rdflib.Graph()
+        g.parse(f, format="json-ld")
+        nt_graph += g
+        print("Processing json-ld file: ", f)
+    except Exception as ex:
+        print("Failed to parse json-ld file:", f)
+        continue
+
+# serialize the graph as n-triples and save to the output directory
+nt_graph.serialize(destination=output_file, format='nt')
+
+print("====================================================================")
+print("created the output concatenated N-Triples file: ", output_file)
